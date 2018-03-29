ccab8e45f802b3eaba085d5b7f7c65d517d60a5b
3chrisprosise <1337074512@qq.com>
2018-03-29 19:24:56 +0800

    添加配置项，优化目录结构

--- pages/test.vue
+++ pages/test.vue
@@ -27,7 +27,7 @@ export default {
     let GetReturn = await this.$axios.$get('/api/test/get');
     let PostReturn = await this.$axios.$post('/api/test/post', {post: 'post'});
     let PutReturn = await this.$axios.$put('/api/test/put', {put: 'put'});
-    let DeleteRerurn = await this.$axios.$delete('/api/test/delete', { data:{delete: 'delete'}})
+    let DeleteRerurn = await this.$axios.$delete('/api/test/delete', { data:{delete: 'delete'}}) // 这边方式稍有不同
 
     this.GetReturn = GetReturn
     this.PostReturn = PostReturn
---
+++ server/config/models.js
@@ -0,0 +1,4 @@
+const Sequelize = require('./database')
+const path = require('path')
+
+const computeApply = Sequelize.import('../models/computeApply')
