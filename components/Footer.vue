9db856f2b5e30002f3a55b8447e7b6ad603a7349
王洋洋 <1028658627@qq.com>
2018-03-29 19:24:52 +0800

    首页以及导航栏

---
+++ components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer class="footer">
+    <p>Clould Device Manage Platform -_-! 简化版 CDMP-NCST - 智慧应用 -  nuxt & koa  - 管理平台</p>
+  </footer>
+</template>
+<style lang="less" scoped>
+.footer{
+  height:60px;
+  p{
+  text-align:center;
+  }
+}
+
+
+</style>
