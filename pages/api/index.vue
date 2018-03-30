52efa0c9058cfbf37dace0f9ddbeb43e0d9c7434
3chrisprosise <1337074512@qq.com>
2018-03-30 18:14:21 +0800

    api封装

--- pages/api/index.vue
+++ pages/api/index.vue
@@ -1,21 +1,43 @@
 <template>
-    <el-row :gutter="20" id="main">
+    <el-row :gutter="0" id="main">
 
       <el-col :span="8" id="left">
-        <el-row clsss="url_input">
-          <el-input placeholder="请输入内容" v-model="input3">
-            <template slot="prepend">Http://</template>
-          </el-input>
+
+        <el-row>
+          <el-col class="input_col">
+            <el-input placeholder="请输入内容" v-model="query_url">
+              <template slot="prepend">Http://</template>
+            </el-input>
+          </el-col>
         </el-row>
+
         <el-row id="js_input">
-          
+          <el-col class="input_col">
+            <el-input
+              type="textarea"
+              :autosize="{ minRows: 30, maxRows: 35}"
+              placeholder="请输入查询的 api json"
+              v-model="input_json">
+            </el-input>
+          </el-col>
         </el-row>
+
       </el-col>
 
       <el-col :span="8" id="center">
         <el-row id="center_button_list">
-
+          <el-col>
+            <el-select v-model="response_data_option" placeholder="数据选项">
+              <el-option
+                v-for="item in data_options"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value">
+              </el-option>
+            </el-select>
+          </el-col>
         </el-row>
+
         <el-row id="response_list">
 
         </el-row>
@@ -48,7 +70,7 @@
 
         .url_input{
           height: auto;
-          border-width: 10px;
+          border-width: 100px;
         }
       }
 
@@ -71,6 +93,22 @@
 
 <script>
   export default {
-
+    data(){
+      return{
+        query_url: '', // 查询的 url 地址
+        input_json: '', // 查询的 json 数据
+        response_data_option: '', //返回数据的选项
+        data_options: [
+          {
+            value: 0,
+            label: '闭合'
+          },
+          {
+            value: 1,
+            label: '展开'
+          }
+        ]
+      }
+    }
   }
 </script>
