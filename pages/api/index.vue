04b89df21247c639fbf916441df08e2db8225b0b
3chrisprosise <1337074512@qq.com>
2018-03-30 20:30:24 +0800

    创建模板，修复bug

--- pages/api/index.vue
+++ pages/api/index.vue
@@ -6,7 +6,7 @@
         <el-row>
           <el-col class="input_col">
             <el-input placeholder="请输入内容" v-model="query_url">
-              <template slot="prepend">Http://</template>
+              <template slot="prepend">{{url_prefix}}</template>
             </el-input>
           </el-col>
         </el-row>
@@ -39,17 +39,55 @@
         </el-row>
 
         <el-row id="response_list">
-
+                    <el-col class="response_data_col">
+            <el-input
+              type="textarea"
+              :autosize="{ minRows: 30, maxRows: 35}"
+              placeholder="这里会显示返回的数据"
+              :disabled="true"
+              v-model="input_json">
+            </el-input>
+          </el-col>
         </el-row>
       </el-col>
 
       <el-col :span="8" id="right" >
         <el-row id="right_button_list">
+            <el-input placeholder="请输入搜索内容" v-model="input5" class="input-with-select">
+              <el-select v-model="select" slot="prepend" placeholder="请选择查询方式">
+                <el-option
+                v-for="item in search_options"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value">
+              </el-option>
+              </el-select>
+              <el-button slot="append" icon="el-icon-search"></el-button>
+            </el-input>
+        </el-row>
 
-        </el-row>
         <el-row id="exist_apis">
-          
+            <el-table
+              :data="exist_api_tableData"
+              border
+              style="width: 100%">
+              <el-table-column
+                prop="api_url"
+                label="api接口">
+              </el-table-column>
+               <el-table-column label="操作">
+                <template slot-scope="scope">
+                  <el-button
+                    size="mini"
+                    @click="handleCopy(scope.$index, scope.row)">复制</el-button>
+                    <el-button
+                    size="mini"
+                    @click="handleDetail(scope.$index, scope.row)">详情</el-button>
+                </template>
+              </el-table-column>
+          </el-table>
         </el-row>
+
       </el-col>
 
     </el-row>
@@ -98,6 +136,7 @@
         query_url: '', // 查询的 url 地址
         input_json: '', // 查询的 json 数据
         response_data_option: '', //返回数据的选项
+        url_prefix: 'http://localhost:3000/', //url前缀
         data_options: [
           {
             value: 0,
@@ -107,7 +146,46 @@
             value: 1,
             label: '展开'
           }
+        ], // 返回数据的操作选项
+        search_options: [
+          {
+            value: 0,
+            label: '数据模型'
+          },
+          {
+            value: 1,
+            label: '特殊接口'
+          }
+        ], //查询数据的操作选项
+        exist_api_tableData: [
+          {
+            id: 1,
+            api_url: 'api/user/get'
+          },
+          {
+            id: 2,
+            api_url: 'api/user/post'
+          },
+          {
+            id: 3,
+            api_url: 'api/user/put'
+          },
+          {
+            id: 4,
+            api_url: 'api/user/delete'
+          },
+          {
+            id: 5,
+            api_url: 'api/device/get'
+          },
         ]
+      } 
+    },
+    methods:{
+
+        async handleCopy (index, row) {
+          this.$message.error('click me !!')
+          this.$message.error('click for Copy' + index + row )
       }
     }
   }
--- server/service/test.js
+++ server/service/test.js
@@ -15,3 +15,12 @@ module.exports.testPut = (ctx) => {
 module.exports.testDelete = (ctx) => {
 
 }
+
+/**
+ * 
+ * @param { JSON } options 相当于查询的条件
+ * @param { Number } limit 限制数据的条数，默认为 comfig 中的条数
+ */
+module.exports.testGetUserData = (options, limit) => {
+
+}
