a5980bdba6fd049dbdbde5b6c83e5edc04c14442
王洋洋 <1028658627@qq.com>
2018-04-23 14:01:14 +0800

    首页

--- pages/index.vue
+++ pages/index.vue
@@ -1,54 +1,80 @@
 <template>
-  <section class="container">
-    <!-- <div>
-      <logo/>
-      <h1 class="title">
-        CDMP
-      </h1>
-      <h2 class="subtitle">
-        云计算及试验设备管理系统 {{ Myname }}
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
-      </div>
-    </div> -->
+  <section class="container"> 
+    <el-row :gutter="20">
+      <el-col :span="8">
+        <div class="grid-content bg-purple">
+          <div class="orderbefore"></div>
+          <span class="order"><i class="el-icon-d-arrow-right"></i> 通知公告</span>
+          <div class="indnotice">
+              <div v-for="info in infoDetail" v-bind:key="info">
+                  <el-col :span="24"><div class="grid-content bg-purple-dark"><a :href='"/notice/" + rule.id'>{{ info.title }} 发布时间：{{ info.releaseDate }} </a></div></el-col>
+              </div>
+          </div>
+        </div>
+      </el-col>
+      <el-col :span="16">
+        <div class="grid-content bg-purple ">
+          <div class="di"><div class="orderbefore"></div>
+                <span class="order"> 
+                  <i class="el-icon-d-arrow-right"></i> 仪器展示</span>
+          </div>
+          <el-carousel class="deviceimg" :interval="5000" type="card" height="200px">
+            <el-carousel-item v-for="item in img" :key="item">
+              <img :src="item.imgFilePath" alt="Nuxt.js Logo" class="Img" />
+            </el-carousel-item>
+          </el-carousel>
+        </div>     
+      </el-col>
+      
+    </el-row>
+    <el-row class="content" :gutter="20">
+      <el-col :span="6">
+        <div class="grid-content bg-purple">
+          <div class="orderbefore"></div>
+          <span class="order"><i class="el-icon-d-arrow-right"></i> 快速通道</span>
+          
+        </div>
+      </el-col>
+      <el-col :span="10"><div class="grid-content bg-purple">
+        
+        </div>
+      </el-col>
+      <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
+    </el-row>
+    <el-row class="last">
+
+    </el-row>
    
-  <el-row :gutter="20">
-    <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
-    <el-col :span="8"><div class="grid-content bg-purple">
-        <el-carousel :interval="5000" arrow="hover" indicator-position="none">
-          <el-carousel-item v-for="item in img" :key="item">
-            <img :src="item.imgFilePath" alt="Nuxt.js Logo" class="Img" />
-          </el-carousel-item>
-        </el-carousel>
-      </div>
-    </el-col>
-    <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
-  </el-row>
-  <div class="middle"> 
-    <el-carousel :interval="4000" type="card" height="300px">
-        <el-carousel-item v-for="item in img" :key="item">
-          <img :src="item.imgFilePath" alt="Nuxt.js Logo" class="Img" />
-        </el-carousel-item>
-    </el-carousel>
-  </div>
-  <el-row class="content" :gutter="20">
-    <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
-    <el-col :span="10"><div class="grid-content bg-purple">
-       
-      </div>
-    </el-col>
-    <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
-  </el-row>
-  <div class="device">
-  </div>
   </section>
 </template>
 
 
 <style lang="less" scoped>
 @border-radius:4px;
+.deviceimg{
+  width:80%;
+  margin:auto;
+}
+.di{
+  width:100%;
+  height:50px;
+}
+.indnotice{
+      width:400px;
+      height:300px;
+  }
+.orderbefore{
+    width: 5px;
+    height:30px;
+    float: left;
+    margin-top:10px;
+    background: #2e2f30;
+}
+.order{
+    display:block;
+    float:left;
+    margin:12px 0 0 20px;
+}
 .el-row {
     margin-top: 20px;
     margin-bottom: 20px;
@@ -81,15 +107,10 @@
 {
    width:100%;
    margin:auto;
-  .middle{
-    height:300px;
-    // background-color: #99a9bf;
-    border-radius: @border-radius;
-  }
-  .device{
+   .last{
     height:200px;
-    border-radius:@border-radius;
-    background-color: burlywood;
+    border-radius: @border-radius;
+    background: #d3dce6;
   }
 }
 .el-carousel__item h3 {
@@ -101,17 +122,18 @@
   }
   
   .el-carousel__item:nth-child(2n) {
-    background-color: #99a9bf;
+    background-color: #fff;
   }
   
   .el-carousel__item:nth-child(2n+1) {
-    background-color: #d3dce6;
+    background-color:  #fff;
   }
   .Img{
-    width: 100%;
+    width: 50%;
     height: 100%;
     background-size:100% 100%; 
   }
+  
  
 </style>
 
