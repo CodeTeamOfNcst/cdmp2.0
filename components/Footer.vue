a0dd50bf5cae2b1d0c85daf9717ce1e7d445ed15
王洋洋 <1028658627@qq.com>
2018-04-24 11:36:14 +0800

    footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <footer class="footer">
-    <p>Clould Device Manage Platform -_-! 简化版 CDMP - 智慧应用 -  nuxt & koa  - 管理平台</p>
+    <p>Clould Device Manage Platform! 简化版 CDMP - 智慧应用 -  nuxt & koa  - 管理平台</p>
   </footer>
 </template>
  
